510bbcb67c1a3299be6c7bd1a41334606cdd8e60
Lance Wilson <86586460+bluefaze360@users.noreply.github.com>
2025-08-23 18:52:18 -0700

    Fixing TypeError list index issue

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def main():
 
         tasks = add_json(task)
 
-        print("Task added successfully with ID {}".format(tasks["tasks"]["id"]))
+        print("Task added successfully with ID {}".format(tasks[-1]["id"]))
         return
     
     elif comm_list[0] == "update":
