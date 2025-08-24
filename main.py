78952e8f4fcf6b657610a8a9c8e64d54850a8483
bluefaze360 <86586460+bluefaze360@users.noreply.github.com>
2025-08-23 22:27:55 -0700

    Delete extra function

--- main.py
+++ main.py
@@ -187,19 +187,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-# Function to append new data to JSON file
-def write_json(new_data, filename='data.json'):
-    with open(filename, 'r+') as file:
-        # Load existing data into a dictionary
-        file_data = json.load(file)
-        
-        # Append new data to the 'emp_details' list
-        file_data["emp_details"].append(new_data)
-        
-        # Move the cursor to the beginning of the file
-        file.seek(0)
-        
-        # Write the updated data back to the file
-        json.dump(file_data, file, indent=4)
