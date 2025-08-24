934e2df4dd458902606ace5d2c4fca629b18a2a2
Lance Wilson <86586460+bluefaze360@users.noreply.github.com>
2025-08-23 17:48:46 -0700

    adding delete

--- main.py
+++ main.py
@@ -15,23 +15,24 @@ This application allows users to manage their tasks via command-line interface.
 
 
 
-
-
-
 import json
 
 def main():
     comm = input("Enter a task: ")
     comm_list = comm.split(" ")
-    if comm == "add":
+    if comm_list[0] == "add":
         print("Adding a task...")
         return
-    elif comm == "update":
+    elif comm_list[0] == "update":
         print("Updating a task...")
+        return
+    elif comm_list[0] == "delete":
+        print("Deleting a task...")
         return
     
 
 if __name__ == "__main__":
     main()
+
 
 
